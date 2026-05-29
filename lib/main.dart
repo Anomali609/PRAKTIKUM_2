dfbaea0054f376a54872a39297bb4e4acd4ae4fa
Anomali609 <dimasyusufalfandhi@gmail.com>
2026-05-29 17:00:52 +0700

    ganti movie screen dengan hotel screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
-import 'screens/movie_screen.dart';
+import 'screens/hotel_screen.dart';
 import 'screens/profile_screen.dart';
 
 void main() => runApp(const MaterialApp(
